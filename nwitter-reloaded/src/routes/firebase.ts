4d4967df1fc23333391a36d3f3f715cb0f6d3b78
dlwhsk0 <hana8145@gmail.com>
2023-10-23 20:01:41 +0900

    #2 SETUP

---
+++ nwitter-reloaded/src/routes/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCru6ix2kKv26MOVcEYl_tMTTRR-Ycolx4",
+  authDomain: "nwitter-e5320.firebaseapp.com",
+  projectId: "nwitter-e5320",
+  storageBucket: "nwitter-e5320.appspot.com",
+  messagingSenderId: "604784976180",
+  appId: "1:604784976180:web:e79d6811ef7521b1108561",
+};
+
+const app = initializeApp(firebaseConfig);
